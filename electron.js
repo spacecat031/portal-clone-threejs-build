676d10e887152385c098f76328443a6a2a9db8dc
Rafael <rafaelluizleitedesal@gmail.com>
2025-01-12 16:59:36 -0400

    first

---
+++ electron.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js'), // Optional
+    },
+  });
+
+  mainWindow.loadURL(
+    process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
